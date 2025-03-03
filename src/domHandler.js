8b46d1d7873103b68d5338dda8be0334b75c7cd9
RSKonkle <rskonkle@gmail.com>
2025-03-03 04:11:13 -0500

    Update html, css files. Add domHandler logic for nav.

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,3 +1,36 @@
-// domManager.js
+// domManager.js - generates landing page content, handles 
 
-console.log("domManager.js running successfully.")
+export function loadPage() {
+    // Initialize content
+    const content = document.getElementById("content");
+
+    // Create left-fixed page nav
+    const nav = document.createElement("nav");
+    nav.id = "nav";
+    
+    // Create page title at top of nav
+    const title = document.createElement("h1");
+    title.id = "title";
+    title.textContent = "Task Master";
+
+    // Create 'Add Task' button in nav
+    const newTaskButton = document.createElement("button");
+    newTaskButton.textContent = "Add Task";
+
+    // Create 'task list' in nav
+    const taskListWrap = document.createElement("div");
+    taskListWrap.id = "task-list";
+    const taskList = document.createElement("ul");
+    const taskListHeader = document.createElement("h3");
+    taskListHeader.textContent = "Your Lists";
+    taskListWrap.appendChild(taskListHeader);
+
+    // Append children to nav
+    nav.appendChild(title);
+    nav.appendChild(newTaskButton);
+    nav.appendChild(taskListWrap);
+
+    // Append nav to content
+    content.appendChild(nav);
+    console.log("loadHome functions successfully.");
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+// import scripts
 import './task.js';
 import './taskManager.js';
 import './domHandler.js';
+import {loadPage} from './domHandler.js';
 
+//import styles
+import './styles.css';
+
+loadPage();
 console.log("index.js running successfully.");
+
+// consider new font for title
+// consider module that creates pop-up introduction for first-time visitors
+// consider offering 'choice of nav color' to personalize page
